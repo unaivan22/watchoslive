fb6ee3b9a7307dd061d7577a7229a09908c1b905
unaivan <unaivan@gmail.com>
2023-06-05 11:36:26 +0700

    update

--- src/page/battery/Battery.js
+++ src/page/battery/Battery.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import WatchLayout from '../../Layout/WatchLayout'
 import ZoomIn from '../../Layout/ZoomIn'
-
+import { MdBattery80 } from 'react-icons/md';
 
 export default function Battery() {
   return (
@@ -9,8 +9,8 @@ export default function Battery() {
         <ZoomIn>
             <section className='grid place-items-center max-h-[190px] h-[190px] max-w-[208px] overflow-x-hidden overflow-y-scroll pt-4 rounded-t-[2.5rem] text-center mx-4'>
                 <div>
-                    <div className="radial-progress text-primary mb-4" style={{"--value":82}}>82%</div>
-                    <h1 className='text-lg font-bold text-zinc-700'>Good</h1>
+                    <div className="radial-progress text-primary mb-4" style={{"--value":82}}><MdBattery80 fontSize="2em" /></div>
+                    <h1 className='text-lg font-bold text-zinc-700'>82%</h1>
                     <p className='text-2xs text-zinc-400'>49 Hours estimate</p>
                 </div>
             </section>
--- src/page/weather/Weather.js
+++ src/page/weather/Weather.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import WatchLayout from '../../Layout/WatchLayout'
 import ZoomIn from '../../Layout/ZoomIn'
-
+import { HiSelector } from 'react-icons/hi';
+import ModalLayout from '../../Layout/ModalLayout';
 
 export default function Weather() {
   return (
@@ -12,9 +13,34 @@ export default function Weather() {
                     {/* <div className="radial-progress text-primary mb-4" style={{"--value":82}}>82%</div> */}
                     <img className='w-24 h-24' src='media/misc/weather.svg' />
                     <h1 className='text-lg font-bold text-zinc-700'>Cloudy</h1>
-                    <p className='text-2xs text-zinc-400'>Surabaya</p>
+                    <label className='flex justify-center mt-1' htmlFor="modalTemplate">
+                      <p className='text-2xs text-zinc-400'>Surabaya</p>
+                      <HiSelector color="gray" fontSize="1em" />
+                    </label>
+                    <div className='absolute top-4 right-4'>
+                      <h1 className='font-bold text-xl text-zinc-700'>28℃</h1>
+                    </div>
                 </div>
             </section>
+
+            <ModalLayout>
+                <div className='flex justify-between flow-row bg-white pointer'>
+                    <p className='absolute top-2 text-xs'>Select City</p>
+                    <label htmlFor="modalTemplate" className="btn btn-xs btn-circle absolute right-4 top-2">✕</label>
+                </div>
+                <section className='max-h-[170px] overflow-y-scroll mt-5 space-y-1'>
+                    <p className='text-xs text-zinc-600'>Surabaya</p>
+                    <p className='text-xs text-zinc-600'>Sidoarjo</p>
+                    <p className='text-xs text-zinc-600'>Mojokerto</p>
+                    <p className='text-xs text-zinc-600'>Gresik</p>
+                    <p className='text-xs text-zinc-600'>Jombang</p>
+                    <p className='text-xs text-zinc-600'>Nganjuk</p>
+                    <p className='text-xs text-zinc-600'>Madiun</p>
+                    <p className='text-xs text-zinc-600'>Magetan</p>
+                </section>
+            </ModalLayout>
+
+
         </ZoomIn>
     </WatchLayout>
   )
